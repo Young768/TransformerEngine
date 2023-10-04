4ff4570a25d36fe003b0a194d161125600beaa95
Donglin Y <dongliny@nvidia.com>
2023-10-03 22:20:12 -0700

    update.

--- tests/jax/test_ln_cp.py
+++ tests/jax/test_ln_cp.py
@@ -39,7 +39,10 @@ def _get_sharding_resource(mesh_names, sharding_type):
 
 
 DEVICE_COUNT = 8
-MESH_CONFIG = [((8,), ("dp",), ShardingType.DP)]
+MESH_CONFIG = [((8,), ("dp",), ShardingType.DP, {"all-reduce":2}),
+              ((4, 2), ("dp", "tp"), ShardingType.DP_TP_COL, {"all-reduce":1}),
+              ((8,), ("tp",), ShardingType.TP_COL, {}),
+              ((2, 4), ("dp", "tp"), ShardingType.DP_TP_COL, {"all-reduce":1}),]
 
 SRS = [
     ShardingResource(),
@@ -48,22 +51,21 @@ SRS = [
     ShardingResource('data', 'model')
 ]
 
+Allreduce = "all-reduce"
 
 class TestShardingMetaGenerator:
 
     BATCH_AXIS_NAME = 'batch'
     MODEL_AXIS_NAME = 'model'
 
-    
 
-
-    @pytest.mark.parametrize('mesh_shape,mesh_names,sharding_type', MESH_CONFIG)
+    @pytest.mark.parametrize('mesh_shape,mesh_names,sharding_type, collective_ref', MESH_CONFIG)
     @pytest.mark.parametrize('input_shape', [(32, 128)])
     @pytest.mark.parametrize('other_shape', [(128,)])
     @pytest.mark.parametrize('batch_dim', [0])
     @pytest.mark.parametrize('zero_centered_gamma', [False])
     @pytest.mark.skipif(not is_devices_enough(DEVICE_COUNT), reason='Num of GPU is not enough')
-    def test_elementwise(self, mesh_shape, mesh_names, sharding_type, input_shape, other_shape,
+    def test_elementwise(self, mesh_shape, mesh_names, sharding_type, collective_ref, input_shape, other_shape,
                          batch_dim, zero_centered_gamma):
 
         epsilon = 1e-6
@@ -73,9 +75,19 @@ class TestShardingMetaGenerator:
                                     epsilon=epsilon, sharding_type=sharding_type, dp_dim_index=batch_dim)
             return jnp.mean(x)
 
-        #def grad_f(x, gamma, beta):
-        #    return jnp.mean(func(x, gamma, beta))
-        test = false
+        def count_collective(hlo):
+            tmp = hlo.splitlines()
+            symb = "-start"
+            result = {}
+            debug = ""
+            for line in tmp:
+               txt = line.split()
+               if len(txt) > 0 and symb in txt[0]:
+                   if Allreduce in txt[0]:
+                       if Allreduce not in result: result[Allreduce] = 0
+                       result[Allreduce] += 1
+            return result
+
         devices = np.asarray(jax.devices()[:DEVICE_COUNT]).reshape(*mesh_shape)
         with global_shard_guard(_get_sharding_resource(mesh_names, sharding_type)):
             with jax.sharding.Mesh(devices, mesh_names):
@@ -84,5 +96,7 @@ class TestShardingMetaGenerator:
                 beta = jnp.ones(other_shape)
                 graded_f = jax.value_and_grad(func, argnums=(0, 1, 2))
                 pjitter = pjit(graded_f)
-                ref_hlo = pjitter.lower(x_, gamma, beta).compile().as_text()
-                # This is for reference to validate the sharding is done correctly.
+                out = {}
+                hlo = pjitter.lower(x_, gamma, beta).compile().as_text()
+                dic = count_collective(hlo)
+                assert dic==collective_ref, f"Expected number of collective is: {dic==collective_ref=}, but got {dic=}."
